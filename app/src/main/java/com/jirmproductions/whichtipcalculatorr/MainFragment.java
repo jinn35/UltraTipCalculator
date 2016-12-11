0b8f88ba95d366b3ba4b56953bf4be9e74ae3817
Josh Inniger <jinniger@gmail.com>
2016-12-11 01:24:49 -0500

    fixed crash

--- app/src/main/java/com/jirmproductions/whichtipcalculatorr/MainFragment.java
+++ app/src/main/java/com/jirmproductions/whichtipcalculatorr/MainFragment.java
@@ -35,10 +35,7 @@ public class MainFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         tipPerPerson = (TextView) rootView.findViewById(R.id.textViewTipPerPerson);
-        tipPerPerson.setText("0.00");
-
         billPerPerson = (TextView) rootView.findViewById(R.id.textViewBillPerPerson);
-        billPerPerson.setText("0.00");
 
         billAmount = (EditText) rootView.findViewById(R.id.billAmountEditText);
         percentToTip = (EditText) rootView.findViewById(R.id.percentToTipEditText);
@@ -48,32 +45,34 @@ public class MainFragment extends Fragment {
 
         calcButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                double bill = Double.parseDouble(billAmount.getText().toString());
-                double percent = Double.parseDouble(percentToTip.getText().toString());
-                double party = Double.parseDouble(numberInParty.getText().toString());
 
-                if(party<=0.0d)
-                {
-                    Toast.makeText(getActivity().getApplicationContext(),
-                            "Please enter a party greater than 0 :-)",
-                            Toast.LENGTH_SHORT).show();
-                }else
-                {
-                    try{
-                        double billPerPersons = bill/party;
-                        double totalTip = billPerPersons*(percent/100);
+                try{
+                    double bill = Double.parseDouble(billAmount.getText().toString());
+                    double percent = Double.parseDouble(percentToTip.getText().toString());
+                    double party = Double.parseDouble(numberInParty.getText().toString());
+
+                    if(party<=0.0d)
+                    {
+                        Toast.makeText(getActivity().getApplicationContext(),
+                                "Please enter a party greater than 0 :-)",
+                                Toast.LENGTH_SHORT).show();
+                    }else {
+
+                        double billPerPersons = bill / party;
+                        double totalTip = billPerPersons * (percent / 100);
                         double totalBill = billPerPersons + totalTip;
 
                         NumberFormat formatter = NumberFormat.getCurrencyInstance();
 
                         tipPerPerson.setText(formatter.format(totalTip));
                         billPerPerson.setText(formatter.format(totalBill));
-
-                    }catch(Exception e)
-                    {
-                        Log.d("error",e.toString());
                     }
 
+                }catch(Exception e)
+                {
+                    Toast.makeText(getActivity().getApplicationContext(),
+                            "Please fill in all fields",
+                            Toast.LENGTH_SHORT).show();
                 }
             }
         });
